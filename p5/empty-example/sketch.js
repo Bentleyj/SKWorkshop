cb782857711518cccaa1e667a2893654ccf6a839
Bentleyj <jgbentley10@gmail.com>
2018-10-05 18:36:14 +0100

    Add p5.js project and dependencies to repo. also setup server and modify empty_example slightly

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,11 @@
+var img;
+function setup() {
+  // put setup code here
+  img = loadImage('assets/2017-10-8-35.jpg');
+}
+
+function draw() {
+  // put drawing code here
+  image(img, 0, 0);
+  // ellipse(50, 50, 50, 100);
+}
