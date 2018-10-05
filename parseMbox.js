bd0479e6d6ae656fd91ee29cc7c515f0e2426d57
Bentleyj <jgbentley10@gmail.com>
2018-10-05 17:08:52 +0100

    Fix date bug and extract all files!

--- parseMbox.js
+++ parseMbox.js
@@ -4,6 +4,9 @@
 	We want to parse our Mbox file to extract the attachments and sort them.
 
 	We're going to use the node-mbox plugin for parsing.
+
+	Format for my images will be: Year-Month-Day-Index.jpg
+
 */
 
 const Mbox = require('node-mbox', { streaming : true });
@@ -17,16 +20,16 @@ mbox.on('message', function(stream) {
 	simpleParser(stream, (err, parsed) => {
 		if(err)
 			throw err;
-		console.log(parsed.date);
-		console.log(parsed.attachments.length);
+		var name = parsed.date.getFullYear() + "-";
+		name += (parsed.date.getMonth() + 1) + "-";
+		name += parsed.date.getDate() + "-";
+		console.log(name);
 		for(var i = 0; i < parsed.attachments.length; i++) {
-			console.log(parsed.attachments[i].contentType);
-			console.log(parsed.attachments[i].filename);
 			if(parsed.attachments[i].contentType === "image/jpeg") {
-				fs.writeFile(__dirname + "/data/Images/" + i + ".jpg", parsed.attachments[i].content, function(err) {
+				fs.writeFile("D:\SKWorkshopImages/" + name + i + ".jpg", parsed.attachments[i].content, function(err) {
 					if(err)
 						throw err;
-					console.log("Done writing: test" + parsed.date.toString());
+					console.log("Done writing:" + name + ".jpg");
 				});
 			}
 		}
