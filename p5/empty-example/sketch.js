0f82499abb2628b195cf8e3f20e255d0035d82dc
Bentleyj <jgbentley10@gmail.com>
2018-10-05 18:42:57 +0100

    Add single image load

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,11 +1,15 @@
 var img;
+
 function setup() {
-  // put setup code here
-  img = loadImage('assets/2017-10-8-35.jpg');
+  	// put setup code here
+  	img = loadImage('assets/2017-10-8-35.jpg');
+  	createCanvas(1920, 1080);
 }
 
 function draw() {
-  // put drawing code here
-  image(img, 0, 0);
-  // ellipse(50, 50, 50, 100);
+	clear();
+  	// put drawing code here
+  	var scale = 3840 / windowWidth;
+  	image(img, 0, 0, windowWidth, 600 / scale);
+    // ellipse(50, 50, 50, 100);
 }
