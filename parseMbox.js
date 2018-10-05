2892e72e02efdff13c41676b85836f4423cb886c
Bentleyj <jgbentley10@gmail.com>
2018-10-05 12:24:51 +0100

    Add mailParser dependency for parsing emails

--- parseMbox.js
+++ parseMbox.js
@@ -7,4 +7,25 @@
 */
 
 const Mbox = require('node-mbox');
+const simpleParser = require('mailparser').simpleParser;
 const fs = require('fs-extra');
+
+const mbox = new Mbox('data/Sent-001.mbox');
+
+mbox.on('message', function(msg) {
+	// 'msg' is a 'Buffer' instance
+	simpleParser(msg, (err, parsed) => {
+		console.log(parsed);
+	});
+	// console.log('got a message', msg.toString());
+});
+
+mbox.on('error', function(err) {
+	console.log('got an error', err);
+});
+
+mbox.on('end', function() {
+	console.log('done reading mbox file');
+});
+
+console.log("Hello, World!");
